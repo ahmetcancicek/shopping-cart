b32be8087465c354ef3d1867c7e0182fa1c3b756
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-10-31 13:04:35 +0300

    Fix configuration

--- src/main/java/com/shopping/config/SpringSecurityConfig.java
+++ src/main/java/com/shopping/config/SpringSecurityConfig.java
@@ -1,12 +1,26 @@
 package com.shopping.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SpringSecurityConfig{
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.admin.username}")
+    private String adminUsername;
+
+    @Value("${spring.admin.password}")
+    private String adminPassword;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
