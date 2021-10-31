f046f02cd5ae6a07ab929f08e23fd9a1dcc59243
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-10-31 12:47:05 +0300

    Add product with model, service, repository

---
+++ src/main/java/com/shopping/model/Product.java
@@ -0,0 +1,35 @@
+package com.shopping.model;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name",nullable = false,unique = true)
+    @Length(min = 3,message = "Name must have at least 5 characters")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price",nullable = false)
+    @DecimalMin(value = "0.00",message = "Price must not to be negative number")
+    public BigDecimal price;
+
+    @Column(name = "quantity")
+    @Min(value = 0, message = "Quantity must not to be negative number")
+    public Integer quantity;
+}
---
+++ src/main/java/com/shopping/servivce/ProductServiceImpl.java
@@ -0,0 +1,31 @@
+package com.shopping.servivce;
+
+import com.shopping.model.Product;
+import com.shopping.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Page<Product> findAllProductsPageable(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+}
