154d1158f334c30342d3595e9bff09d7067059ba
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-10-31 11:13:29 +0300

    Implementation service for user and customer

---
+++ src/main/java/com/shopping/servivce/CustomerServiceImpl.java
@@ -0,0 +1,29 @@
+package com.shopping.servivce;
+
+import com.shopping.model.Customer;
+import com.shopping.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.saveAndFlush(customer);
+    }
+}
---
+++ src/main/java/com/shopping/servivce/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.shopping.servivce;
+
+import com.shopping.model.User;
+import com.shopping.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        return userRepository.saveAndFlush(user);
+    }
+}
