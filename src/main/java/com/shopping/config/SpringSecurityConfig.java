54238b4b50137a7d7ce157099b9c5148e709e72e
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-11-02 22:06:12 +0300

    Disable security

--- src/main/java/com/shopping/config/SpringSecurityConfig.java
+++ src/main/java/com/shopping/config/SpringSecurityConfig.java
@@ -19,7 +19,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
+        http.csrf().disable().httpBasic().disable();
     }
 
     @Bean
