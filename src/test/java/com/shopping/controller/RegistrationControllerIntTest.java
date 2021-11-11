1a28b38d9a6a04f72725ab1d6be2ad9d07417cb9
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-11-11 19:27:15 +0300

    Add unit and integration test for register

---
+++ src/test/java/com/shopping/controller/RegistrationControllerIntTest.java
@@ -0,0 +1,45 @@
+package com.shopping.controller;
+
+import com.shopping.model.Customer;
+import com.shopping.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RegistrationControllerIntTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    public void should_be_return_created_customer_when_called_byRegisterCustomer() {
+        Customer customer = Customer.builder()
+                .firstName("First Name")
+                .lastName("Last Name")
+                .users(new User("email@email.com", "username", "password", true))
+                .build();
+
+
+        ResponseEntity<Customer> postCustomer = restTemplate.exchange("/registration",
+                HttpMethod.POST,
+                new HttpEntity<>(customer,headers),
+                Customer.class);
+
+        assertNotNull(postCustomer);
+        assertEquals(postCustomer.getStatusCode(), HttpStatus.CREATED);
+        assertEquals(postCustomer.getBody().getFirstName(), "First Name");
+
+    }
+
+}
---
+++ src/test/java/com/shopping/controller/RegistrationControllerTest.java
@@ -0,0 +1,80 @@
+package com.shopping.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shopping.model.Customer;
+import com.shopping.model.User;
+import com.shopping.servivce.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(RegistrationController.class)
+class RegistrationControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    CustomerService customerService;
+
+    @Test
+    void should_return_successful_when_body_isValid() throws Exception {
+        Customer customer = Customer.builder()
+                .id(1L)
+                .firstName("First Name")
+                .lastName("Last Name")
+                .users(new User("email@email.com", "username", "password", true))
+                .build();
+
+        Mockito.when(customerService.saveCustomer(customer)).thenReturn(customer);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(customer));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstName", is("First Name")));
+    }
+
+
+    @Test
+    void should_return_bad_request_when_body_isNotValid() throws Exception {
+        Customer customer = Customer.builder()
+                .id(1L)
+                .firstName("First Name")
+                .lastName("Last Name")
+                .users(new User())
+                .build();
+
+        Mockito.when(customerService.saveCustomer(customer)).thenReturn(customer);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(customer));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+}
