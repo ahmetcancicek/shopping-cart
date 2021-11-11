d269cab5f6cf4a8feacb7ebe1bb1fd54723e8343
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-11-11 19:26:59 +0300

    Renamed controller and add register endpoint

---
+++ src/main/java/com/shopping/controller/RegistrationController.java
@@ -0,0 +1,28 @@
+package com.shopping.controller;
+
+import com.shopping.model.Customer;
+import com.shopping.servivce.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public RegistrationController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Customer> registerCustomer(@Valid @RequestBody Customer customer) {
+        return new ResponseEntity<Customer>(customerService.saveCustomer(customer), HttpStatus.CREATED);
+    }
+}
