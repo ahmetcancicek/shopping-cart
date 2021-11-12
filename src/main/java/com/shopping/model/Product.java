5baefbffa47d91ea9bbf641ea9d908bb20432e2b
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-11-12 21:59:22 +0300

    Fix model

--- src/main/java/com/shopping/model/Product.java
+++ src/main/java/com/shopping/model/Product.java
@@ -1,6 +1,9 @@
 package com.shopping.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -11,6 +14,9 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "product")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
