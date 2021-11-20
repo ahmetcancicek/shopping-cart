ca664f13b36da6878c172a188875b072def1046b
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-11-20 22:57:13 +0300

    Remove cart implementation

--- src/main/java/com/shopping/servivce/CartService.java
+++ src/main/java/com/shopping/servivce/CartService.java
@@ -1,17 +1,5 @@
 package com.shopping.servivce;
 
-import com.shopping.model.Product;
-
-import java.math.BigDecimal;
-import java.util.Map;
-
 public interface CartService {
 
-    void addProduct(Product product);
-
-    void removeProduct(Product product);
-
-    Map<Product,Integer> getProductsInCart();
-
-    BigDecimal getTotal();
 }
--- src/main/java/com/shopping/servivce/impl/CartServiceImpl.java
+++ src/main/java/com/shopping/servivce/impl/CartServiceImpl.java
@@ -1,56 +1,9 @@
 package com.shopping.servivce.impl;
 
-import com.shopping.model.Product;
-import com.shopping.repository.ProductRepository;
 import com.shopping.servivce.CartService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class CartServiceImpl implements CartService {
 
-    private final ProductRepository productRepository;
-
-    private Map<Product, Integer> products = new HashMap<>();
-
-    @Autowired
-    public CartServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    @Override
-    public void addProduct(Product product) {
-        if (products.containsKey(product)) {
-            products.replace(product, products.get(product) + 1);
-        } else
-            products.put(product, 1);
-    }
-
-    @Override
-    public void removeProduct(Product product) {
-        if (products.containsKey(product)) {
-            if (products.get(product) > 1)
-                products.replace(product, products.get(product) - 1);
-            else if (products.get(product) == 1) {
-                products.remove(product);
-            }
-        }
-    }
-
-    @Override
-    public Map<Product, Integer> getProductsInCart() {
-        return Collections.unmodifiableMap(products);
-    }
-
-    @Override
-    public BigDecimal getTotal() {
-        return products.entrySet().stream()
-                .map(x -> x.getKey().getPrice().multiply(BigDecimal.valueOf(x.getValue())))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
 }
