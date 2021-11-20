5e68de2a1b45353594fa8503bbf145fcb523a7ca
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-11-20 16:43:28 +0300

    Add new model

---
+++ src/main/java/com/shopping/model/Cart.java
@@ -0,0 +1,30 @@
+package com.shopping.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
+@Data
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "total_price",nullable = false)
+    @DecimalMin(value = "0.00",message = "Price must not to be negative number")
+    private BigDecimal totalPrice;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    @JsonIgnore
+    private Customer customer;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<CartItem> cartItems;
+}
---
+++ src/main/java/com/shopping/model/CartItem.java
@@ -0,0 +1,36 @@
+package com.shopping.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart_item")
+@Data
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    @Min(value = 0, message = "Quantity must not to be negative number")
+    private Integer quantity;
+
+    @Column(name = "price",nullable = false)
+    @DecimalMin(value = "0.00",message = "Price must not to be negative number")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    @JsonIgnore
+    private Cart cart;
+}
