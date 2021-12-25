a522459f76f59acefda8796b8aa6695adc2ae00a
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-12-25 19:08:00 +0300

    Refactored repository for cart

--- src/main/java/com/shopping/repository/CartRepository.java
+++ src/main/java/com/shopping/repository/CartRepository.java
@@ -1,9 +1,9 @@
 package com.shopping.repository;
 
 import com.shopping.model.Cart;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends CrudRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
