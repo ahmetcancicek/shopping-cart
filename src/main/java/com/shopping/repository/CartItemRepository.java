2bdef05a9d7d4690e6f1a336fd00ab45ca5e774a
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2021-12-25 19:43:35 +0300

    Refactored repository

--- src/main/java/com/shopping/repository/CartItemRepository.java
+++ src/main/java/com/shopping/repository/CartItemRepository.java
@@ -2,10 +2,11 @@ package com.shopping.repository;
 
 import com.shopping.model.Cart;
 import com.shopping.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemRepository extends CrudRepository<CartItem,Long> {
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     void deleteByCart(Cart cart);
 }
