ef8e93f8da2d96fcea76174d15697e2933252588
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-03-17 23:13:05 +0300

    Refactor cart

--- src/main/java/com/shopping/repository/CartRepository.java
+++ src/main/java/com/shopping/repository/CartRepository.java
@@ -1,9 +1,13 @@
 package com.shopping.repository;
 
 import com.shopping.model.Cart;
+import com.shopping.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByCustomer(Customer customer);
 }
--- src/main/java/com/shopping/service/CartService.java
+++ src/main/java/com/shopping/service/CartService.java
@@ -1,9 +1,31 @@
 package com.shopping.service;
 
 import com.shopping.model.Cart;
+import com.shopping.model.Customer;
+import com.shopping.model.Product;
 
 import java.util.Optional;
 
 public interface CartService {
     Optional<Cart> findById(Long id);
+
+    Optional<Cart> findByUsername(String username);
+
+    Optional<Cart> findByCustomerId(Long id);
+
+    Optional<Cart> findByCustomer(Customer customer);
+
+    void addItemToCart(Customer customer, Product product);
+
+    void addItemToCart(Customer customer, Long productId);
+
+    void addItemToCart(Customer customer, Product product, int quantity);
+
+    void addItemToCart(Long customerId, Long productId);
+
+    void removeAllCartItems(Customer customer);
+
+    void removeAllCartItems(Cart cart);
+
+    void removeItem(Customer customer, Product product);
 }
