baeec24c8aa112b850f23ae0f73602548d2e1299
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-04-10 21:28:11 +0300

    Add cart implementation

--- src/main/java/com/shopping/service/impl/CartServiceImpl.java
+++ src/main/java/com/shopping/service/impl/CartServiceImpl.java
@@ -1,25 +1,157 @@
 package com.shopping.service.impl;
 
-import com.shopping.model.Cart;
+import com.shopping.exception.ProductNotFoundException;
+import com.shopping.exception.UserNotFoundException;
+import com.shopping.model.*;
 import com.shopping.repository.CartRepository;
 import com.shopping.service.CartService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.shopping.service.CustomerService;
+import com.shopping.service.ProductService;
+import com.shopping.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class CartServiceImpl implements CartService {
 
     private final CartRepository cartRepository;
+    private final CustomerService customerService;
+    private final ProductService productService;
+    private final UserService userService;
 
-    @Autowired
-    public CartServiceImpl(CartRepository cartRepository) {
+    public CartServiceImpl(CartRepository cartRepository, CustomerService customerService, ProductService productService, UserService userService) {
         this.cartRepository = cartRepository;
+        this.customerService = customerService;
+        this.productService = productService;
+        this.userService = userService;
     }
 
     @Override
     public Optional<Cart> findById(Long id) {
         return cartRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Cart> findByUsername(String username) {
+        Optional<User> user = userService.findByUsername(username);
+        if (user.isEmpty()) {
+            log.error("user does not exists: {}" + username);
+            throw new UserNotFoundException("user does not exists: {" + username + "}");
+        }
+
+        return findByCustomer(user.get().getCustomer());
+    }
+
+    @Override
+    public Optional<Cart> findByCustomerId(Long id) {
+        return cartRepository.findByCustomer_Id(id);
+    }
+
+
+    @Override
+    public Optional<Cart> findByCustomer(Customer customer) {
+        return cartRepository.findByCustomer(customer);
+    }
+
+    @Override
+    public void addItemToCart(Customer customer, Product product) {
+        addItemToCart(customer.getId(), product.getId(), 1);
+    }
+
+
+    @Override
+    public void addItemToCart(Customer customer, Long productId) {
+        addItemToCart(customer.getId(), productId, 1);
+    }
+
+    @Override
+    public void addItemToCart(Customer customer, Product product, int quantity) {
+        addItemToCart(customer.getId(), product.getId(), quantity);
+    }
+
+    @Override
+    public void addItemToCart(Long customerId, Long productId, int quantity) {
+        Optional<Customer> optionalCustomer = customerService.findById(customerId);
+        if (optionalCustomer.isEmpty())
+            throw new UserNotFoundException("customer does not exist with this customerId");
+
+        Optional<Product> optionalProduct = productService.findById(productId);
+        if (optionalProduct.isEmpty())
+            throw new ProductNotFoundException("product does not exit with this productId");
+
+        updateCart(optionalCustomer.get(), optionalProduct.get(), quantity);
+    }
+
+    @Override
+    public void addItemToCart(Long customerId, Long productId) {
+        addItemToCart(customerId, productId, 1);
+    }
+
+    @Override
+    public void removeAllCartItems(Customer customer) {
+        Optional<Customer> optionalCustomer = customerService.findById(customer.getId());
+        if (optionalCustomer.isEmpty())
+            throw new UserNotFoundException("customer does not exist with this customerId");
+
+        customer.getCart().getItems().removeIf(cartItem -> cartItem.getId() != null);
+
+        cartRepository.save(customer.getCart());
+        log.info("all items of the cart has been deleted: {}", customer.getCart().getId());
+    }
+
+    @Override
+    public void removeAllCartItems(Cart cart) {
+        Optional<Cart> optionalCart = cartRepository.findById(cart.getId());
+        if (optionalCart.isEmpty())
+            throw new IllegalArgumentException();
+
+        cart.getItems().removeIf(cartItem -> cartItem.getId() != null);
+
+        cartRepository.save(cart);
+        log.info("all items of the cart has been deleted: {}", cart.getId());
+    }
+
+    @Override
+    public void removeItem(Customer customer, Product product) {
+        Optional<Customer> optionalCustomer = customerService.findById(customer.getId());
+        if (optionalCustomer.isEmpty())
+            throw new UserNotFoundException("customer does not exist with this customerId");
+
+        Optional<Product> optionalProduct = productService.findById(product.getId());
+        if (optionalProduct.isEmpty())
+            throw new ProductNotFoundException("product does not exist with this productId");
+
+        Long productId = optionalProduct.get().getId();
+        customer.getCart().getItems().removeIf(cartItem -> cartItem.getProduct().getId().equals(productId));
+    }
+
+    private void updateCart(Customer customer, Product product, int quantity) {
+        Optional<Cart> optionalCart = cartRepository.findByCustomer(customer);
+
+        Cart customerCart = optionalCart.map(cart -> {
+            Optional<CartItem> optionalCartItem = Optional.of(cart.findItem(product).map(cartItem -> {
+                cartItem.setQuantity((cartItem.getQuantity() + quantity));
+                return cartItem;
+            }).orElse(CartItem.builder()
+                    .cart(cart)
+                    .product(product)
+                    .quantity(quantity)
+                    .build()));
+
+            CartItem cartItem = optionalCartItem.get();
+
+            cart.updateItem(cartItem);
+            customer.setCart(cart);
+            cart.setCustomer(customer);
+            return cart;
+        }).orElse(Cart.builder()
+                .customer(customer)
+                .build());
+
+        cartRepository.save(customerCart);
+        log.info("cart has been updated: {}", customerCart.getCustomer().getCart().getId());
     }
 }
---
+++ src/test/java/com/shopping/service/impl/CartServiceImplTest.java
@@ -0,0 +1,190 @@
+package com.shopping.service.impl;
+
+import com.shopping.model.*;
+import com.shopping.repository.CartRepository;
+import com.shopping.service.CustomerService;
+import com.shopping.service.ProductService;
+import com.shopping.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CartServiceImplTest {
+
+    @Mock
+    private CartRepository cartRepository;
+
+    @Mock
+    private CustomerService customerService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ProductService productService;
+
+    @InjectMocks
+    private CartServiceImpl cartService;
+
+    private User user;
+    private Customer customer;
+    private Product product;
+    private Cart cart;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .id(1L)
+                .username("username")
+                .email("email@email.com")
+                .active(true)
+                .password("password")
+                .build();
+
+        customer = Customer.builder()
+                .id(2L)
+                .user(user)
+                .firstName("First Name")
+                .lastName("Last Name")
+                .build();
+
+        product = Product.builder()
+                .name("Product")
+                .quantity(10)
+                .price(BigDecimal.valueOf(10))
+                .id(10L)
+                .build();
+
+        cart = Cart.builder()
+                .id(5L)
+                .customer(customer)
+                .build();
+
+        CartItem cartItem = CartItem.builder()
+                .id(3L)
+                .cart(cart)
+                .product(product)
+                .quantity(1)
+                .price(BigDecimal.valueOf(10))
+                .build();
+
+        cart.addItem(cartItem);
+        user.setCustomer(customer);
+        customer.setCart(cart);
+    }
+
+    @Test
+    void it_should_return_cart_of_that_username() {
+        // given
+        String username = user.getUsername();
+        when(userService.findByUsername(any(String.class))).thenReturn(Optional.of(user));
+        when(cartRepository.findByCustomer(any(Customer.class))).thenReturn(Optional.of(cart));
+
+        // when
+        Optional<Cart> expectedCart = cartService.findByUsername(username);
+
+        // then
+        verify(cartRepository, times(1)).findByCustomer(any(Customer.class));
+        assertTrue(expectedCart.isPresent(), "Returned must no be null");
+    }
+
+    @Test
+    void it_should_return_cart_of_that_customer_id() {
+        // given
+        Long customerId = customer.getId();
+        Long cartId = cart.getId();
+        when(cartRepository.findByCustomer_Id(any())).thenReturn(Optional.of(cart));
+
+        // when
+        Optional<Cart> expectedCart = cartService.findByCustomerId(customerId);
+
+        // then
+        verify(cartRepository, times(1)).findByCustomer_Id(customerId);
+        assertTrue(expectedCart.isPresent(), "Returned must not be null");
+        assertEquals(expectedCart.get().getId(), cartId);
+    }
+
+    @Test
+    void it_should_return_cart_of_that_customer() {
+        // given
+        Long cartId = 5L;
+        when(cartRepository.findByCustomer(any(Customer.class))).thenReturn(Optional.of(cart));
+
+        // when
+        Optional<Cart> expectedCart = cartService.findByCustomer(customer);
+
+        // then
+        verify(cartRepository, times(1)).findByCustomer(customer);
+        assertTrue(expectedCart.isPresent(), "Returned must not be null");
+        assertEquals(expectedCart.get().getId(), cartId, "CartId must be equal");
+    }
+
+    @Test
+    void it_should_add_item_to_cart_with_customer_and_product() {
+        // given
+        Long customerId = customer.getId();
+        Long productId = product.getId();
+        when(customerService.findById(any())).thenReturn(Optional.of(customer));
+        when(productService.findById(any())).thenReturn(Optional.of(product));
+        when(cartRepository.save(any())).thenReturn(cart);
+        when(cartRepository.findByCustomer(any())).thenReturn(Optional.of(cart));
+
+        // when
+        cartService.addItemToCart(customer, product);
+
+        // then
+        verify(cartRepository, times(1)).save(any(Cart.class));
+        verify(cartRepository, times(1)).findByCustomer(any(Customer.class));
+    }
+
+
+    @Test
+    void it_should_remove_all_cart_items_with_customer() {
+        // given
+        when(customerService.findById(any())).thenReturn(Optional.of(customer));
+
+        // when
+        cartService.removeAllCartItems(customer);
+
+        // then
+        verify(cartRepository, times(1)).save(any(Cart.class));
+        assertEquals(0, customer.getCart().getItems().size());
+    }
+
+    @Test
+    void it_should_remove_all_cart_items_with_cart() {
+        // given
+        when(cartRepository.findById(any())).thenReturn(Optional.of(cart));
+
+        // when
+        cartService.removeAllCartItems(cart);
+
+        // then
+        verify(cartRepository, times(1)).save(any());
+        assertEquals(0, customer.getCart().getItems().size());
+    }
+
+    @Test
+    void it_should_remove_item_from_cart() {
+        // given
+        when(customerService.findById(any())).thenReturn(Optional.ofNullable(customer));
+        when(productService.findById(any())).thenReturn(Optional.ofNullable(product));
+
+        // when
+        cartService.removeItem(customer, product);
+
+        // then
+        assertEquals(0, customer.getCart().getItems().size());
+
+    }
+}
