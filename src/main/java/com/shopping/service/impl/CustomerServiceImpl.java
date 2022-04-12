54da4528fda043978645851787f79c3964a37210
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-04-12 09:00:29 +0300

    Fix check if customer does not already exist

--- src/main/java/com/shopping/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/shopping/service/impl/CustomerServiceImpl.java
@@ -1,14 +1,18 @@
 package com.shopping.service.impl;
 
+import com.shopping.exception.UserAlreadyExistsException;
 import com.shopping.model.Cart;
 import com.shopping.model.Customer;
 import com.shopping.model.User;
 import com.shopping.repository.CartRepository;
 import com.shopping.repository.CustomerRepository;
+import com.shopping.repository.UserRepository;
 import com.shopping.service.CustomerService;
+import com.shopping.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,10 +22,12 @@ import java.util.Optional;
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, UserRepository userRepository) {
         this.customerRepository = customerRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -31,6 +37,8 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer save(Customer customer) {
+        isExist(customer.getUser());
+
         if (customer.getCart() == null)
             customer.setCart(Cart.builder().customer(customer).build());
 
@@ -38,6 +46,21 @@ public class CustomerServiceImpl implements CustomerService {
         log.info("new customer has been created: {}", savedCustomer.getFirstName());
         return savedCustomer;
     }
+
+    private void isExist(User user) {
+        Optional<User> existing = userRepository.findByEmail(user.getEmail());
+        existing.ifPresent(it -> {
+            log.error("user already exists: " + it.getEmail());
+            throw new UserAlreadyExistsException("user already exists: {}" + it.getEmail());
+        });
+
+        existing = userRepository.findByUsername(user.getUsername());
+        existing.ifPresent(it -> {
+            log.error("user already exists: " + it.getUsername());
+            throw new UserAlreadyExistsException("user already exists: {}" + it.getUsername());
+        });
+    }
+
 
     @Override
     public Optional<Customer> findById(Long id) {
