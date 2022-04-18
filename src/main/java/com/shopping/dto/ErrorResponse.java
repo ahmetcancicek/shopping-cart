83552905e108e8b21966ba3cd77741a3c50743b3
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-04-18 23:08:30 +0300

    Add dto for response to return error

---
+++ src/main/java/com/shopping/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.shopping.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+
+    private HttpStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private String message;
+}
