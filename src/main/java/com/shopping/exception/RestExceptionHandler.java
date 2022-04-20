dee52b3f03846311eed72c90afef4c6da55b4410
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-04-20 13:56:10 +0300

    Add exception handler for response

---
+++ src/main/java/com/shopping/exception/RestExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.shopping.exception;
+
+import com.shopping.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    protected ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        // TODO: Add message
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("")
+                .status(HttpStatus.BAD_GATEWAY)
+                .build();
+        return buildResponseEntity(errorResponse);
+    }
+
+    protected ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
+        // TODO: Add message
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("")
+                .status(HttpStatus.BAD_REQUEST)
+                .build();
+        return buildResponseEntity(errorResponse);
+    }
+
+    @ExceptionHandler(javax.validation.ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolation(javax.validation.ConstraintViolationException ex) {
+        // TODO: Add constraint validation errors list
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message("Validation error")
+                .status(HttpStatus.BAD_GATEWAY)
+                .build();
+        return buildResponseEntity(ErrorResponse.builder().build());
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ErrorResponse errorResponse) {
+        return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
+    }
+}
