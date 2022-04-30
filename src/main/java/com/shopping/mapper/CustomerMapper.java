c348e5aa8e6970d8786ee65f2a87908f8b772982
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-04-30 12:25:58 +0300

    Fix mapper for customer

--- src/main/java/com/shopping/mapper/CustomerMapper.java
+++ src/main/java/com/shopping/mapper/CustomerMapper.java
@@ -13,11 +13,23 @@ public interface CustomerMapper {
 
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
+    @Mapping(source = "user.email", target = "email")
+    @Mapping(source = "user.password", target = "password")
+    @Mapping(source = "user.username", target = "username")
     CustomerPayload toCustomerPayload(Customer customer);
 
+    @Mapping(source = "email", target = "user.email")
+    @Mapping(source = "username", target = "user.username")
+    @Mapping(source = "password", target = "user.password")
     Customer toCustomer(CustomerPayload customerPayload);
 
+    @Mapping(source = "user.email", target = "email")
+    @Mapping(source = "user.password", target = "password")
+    @Mapping(source = "user.username", target = "username")
     List<CustomerPayload> toCustomerPayloads(List<Customer> customers);
 
+    @Mapping(source = "email", target = "user.email")
+    @Mapping(source = "username", target = "user.username")
+    @Mapping(source = "password", target = "user.password")
     List<Customer> toCustomers(List<CustomerPayload> customerPayloads);
 }
