f75cbf5063fd33fa8eaae03e9f83bd3819b17cb6
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-04-30 17:39:48 +0300

    Create order service and implementation

--- src/main/java/com/shopping/service/OrderService.java
+++ src/main/java/com/shopping/service/OrderService.java
@@ -1,22 +1,5 @@
 package com.shopping.service;
 
-import com.shopping.model.Customer;
-import com.shopping.model.Order;
-import com.shopping.model.Payment;
-
 public interface OrderService {
 
-    Order save(Order order);
-
-    Order createOrder(Customer customer);
-
-    Order cancel(Order order);
-
-    Order cancel(Long id);
-
-    Order complete(Order order);
-
-    Order complete(Long id);
-
-    Order applyPayment(Order order, Payment payment);
 }
--- src/main/java/com/shopping/service/impl/OrderServiceImpl.java
+++ src/main/java/com/shopping/service/impl/OrderServiceImpl.java
@@ -1,45 +1,8 @@
 package com.shopping.service.impl;
 
-import com.shopping.model.Customer;
-import com.shopping.model.Order;
-import com.shopping.model.Payment;
 import com.shopping.service.OrderService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    @Override
-    public Order save(Order order) {
-        return null;
-    }
-
-    @Override
-    public Order createOrder(Customer customer) {
-        return null;
-    }
-
-    @Override
-    public Order cancel(Order order) {
-        return null;
-    }
-
-    @Override
-    public Order cancel(Long id) {
-        return null;
-    }
-
-    @Override
-    public Order complete(Order order) {
-        return null;
-    }
-
-    @Override
-    public Order complete(Long id) {
-        return null;
-    }
-
-    @Override
-    public Order applyPayment(Order order, Payment payment) {
-        return null;
-    }
 }
