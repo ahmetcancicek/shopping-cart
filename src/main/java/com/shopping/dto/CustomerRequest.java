f11f7ed0f93a55d43ffc854c3420e407f0d6cb2b
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-04 12:00:31 +0300

    Fix DTO

--- src/main/java/com/shopping/dto/CustomerRequest.java
+++ src/main/java/com/shopping/dto/CustomerRequest.java
@@ -11,23 +11,18 @@ import javax.validation.constraints.NotEmpty;
 @Data
 @Builder
 public class CustomerRequest {
-    @Column(name = "first_name", nullable = true)
     private String firstName;
 
-    @Column(name = "last_name", nullable = true)
     private String lastName;
 
-    @Column(name = "email", unique = true, nullable = false)
     @Email(message = "Email must be valid!")
     @NotEmpty(message = "Email must not be empty")
     private String email;
 
-    @Column(name = "username", unique = true, nullable = false)
     @Length(min = 5, message = "Username must have at least 5 characters")
     @NotEmpty(message = "Username must not be empty")
     private String username;
 
-    @Column(name = "password", nullable = false)
     @Length(min = 5, message = "Password must have at least 5 characters")
     @NotEmpty(message = "Password must not be empty")
     private String password;
--- src/main/java/com/shopping/dto/ProductResponse.java
+++ src/main/java/com/shopping/dto/ProductResponse.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Column;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
@@ -13,24 +12,19 @@ import java.math.BigDecimal;
 @Data
 @Builder
 public class ProductResponse {
-    @Column(name = "serial_number", nullable = false, unique = true)
     @Length(min = 3, message = "Serial number have at least 3 characters")
     @NotEmpty(message = "Serial number must not be empty")
     private String serialNumber;
 
-    @Column(name = "name", nullable = false)
     @Length(min = 3, message = "Name must have at least 5 characters")
     @NotEmpty(message = "Name must not be empty")
     private String name;
 
-    @Column(name = "description")
     private String description;
 
-    @Column(name = "price", nullable = false)
     @DecimalMin(value = "0.00", message = "Price must not to be negative number")
     public BigDecimal price;
 
-    @Column(name = "quantity")
     @Min(value = 0, message = "Quantity must not to be negative number")
     public Integer quantity;
 }
