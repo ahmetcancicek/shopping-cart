1e555a6ce32f17815d4e72540a9051b1adb36072
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-08 10:39:30 +0300

    Add insert statement for integration test

--- src/test/resources/clear.sql
+++ src/test/resources/clear.sql
@@ -1,4 +1,5 @@
 ----------------------------------------- ## DELETED FOR RegistrationControllerIntTest ## --------------------------------
+DELETE FROM cart_item WHERE ID=2000;
 DELETE FROM cart WHERE ID=1000 OR ID=2000;
 DELETE FROM customer WHERE ID=1000 OR ID=2000;
 DELETE FROM users WHERE ID=1000 OR ID=2000;
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,3 +1,7 @@
+----------------------------------------- ## GENERATED FOR ProductControllerIntTest ## --------------------------------
+insert into product (id, serial_number, description, name, price, quantity) values (3000, "KMNA239", "Apple iPhone 13 PRO", "iPhone 13 PRO", 1000, 5);
+insert into product (id, serial_number, description, name, price, quantity) values (3001, "PADMA232", "Samsung Galaxy S22", "Galaxy S22", 1100, 3);
+
 ----------------------------------------- ## GENERATED FOR RegistrationControllerIntTest ## --------------------------------
 insert into users(id,active, email, password, username) values(1000,1,"lucycar@email.com","9adm3av","lucycar");
 insert into customer(id,first_name, last_name, user_id) values(1000,"Lucy","Car",(SELECT id FROM users WHERE email="lucycar@email.com"));
@@ -6,7 +10,7 @@ insert into cart(id, total_price, customer_id) values(1000,0,(SELECT id FROM cus
 insert into users(id,active, email, password, username) values(2000,1,"billking@email.com","j7aad3","billking");
 insert into customer(id,first_name, last_name, user_id) values(2000,"Bill","King",(SELECT id FROM users WHERE email="billking@email.com"));
 insert into cart(id, total_price, customer_id) values(2000,0,(SELECT id FROM customer WHERE user_id=(SELECT id FROM users WHERE email="billking@email.com")));
+insert into cart_item (id, price, quantity, cart_id, product_id) values (2000,1000,1,
+                                                                         (SELECT id FROM cart WHERE customer_id=((SELECT id FROM users WHERE email="billking@email.com"))),
+                                                                         (SELECT id FROM product WHERE serial_number='KMNA239'));
 
------------------------------------------ ## GENERATED FOR ProductControllerIntTest ## --------------------------------
-insert into product (id, serial_number, description, name, price, quantity) values (3000, "KMNA239", "Apple iPhone 13 PRO", "iPhone 13 PRO", 1000, 5);
-insert into product (id, serial_number, description, name, price, quantity) values (3001, "PADMA232", "Samsung Galaxy S22", "Galaxy S22", 1100, 3);
