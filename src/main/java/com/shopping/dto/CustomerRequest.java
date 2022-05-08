a3252052a96f6257168fb07049d5b87aa5f9361e
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-08 11:12:24 +0300

    Fix DTO

--- src/main/java/com/shopping/dto/CustomerRequest.java
+++ src/main/java/com/shopping/dto/CustomerRequest.java
@@ -3,12 +3,26 @@ package com.shopping.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 public class CustomerRequest {
     private String firstName;
+
     private String lastName;
+
+    @Email(message = "Email must be valid!")
+    @NotEmpty(message = "Email must not be empty")
     private String email;
+
+    @Size(min = 5, message = "Username must have at least 5 characters")
+    @NotEmpty(message = "Username must not be empty")
     private String username;
+
+    @Size(min = 5, message = "Password must have at least 5 characters")
+    @NotEmpty(message = "Password must not be empty")
     private String password;
 }
