e402623560be85a126015785c2f4f636e4d9a396
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-14 23:13:40 +0300

    Add initialize data for IT

--- src/test/resources/clear.sql
+++ src/test/resources/clear.sql
@@ -1,9 +1,20 @@
------------------------------------------ ## DELETED FOR RegistrationControllerIntTest ## --------------------------------
-DELETE FROM cart_item WHERE ID=2000;
-DELETE FROM cart WHERE ID=1000 OR ID=2000;
-DELETE FROM customer WHERE ID=1000 OR ID=2000;
-DELETE FROM users WHERE ID=1000 OR ID=2000;
+----------------------------------------- ## DELETED FOR USER and CUSTOMER ## --------------------------------
+DELETE FROM cart_item WHERE ID=1000;
+DELETE FROM cart WHERE ID=1000;
+DELETE FROM users_roles WHERE user_id=1000;
+DELETE FROM customer WHERE ID=1000;
+DELETE FROM users WHERE ID=1000;
 
------------------------------------------ ## DELETED FOR ProductontrollerIntTest ## --------------------------------
+DELETE FROM cart_item WHERE ID=2000;
+DELETE FROM cart WHERE ID=2000;
+DELETE FROM users_roles WHERE user_id=2000;
+DELETE FROM customer WHERE  ID=2000;
+DELETE FROM users WHERE ID=2000;
+
+----------------------------------------- ## DELETE FOR ROLE ## --------------------------------
+DELETE FROM role WHERE role="USER";
+DELETE FROM role WHERE role="ADMIN";
+
+----------------------------------------- ## DELETED FOR PRODUCT ## --------------------------------
 DELETE FROM product WHERE ID=3000;
 DELETE FROM product WHERE ID=3001;
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,16 +1,23 @@
------------------------------------------ ## GENERATED FOR ProductControllerIntTest ## --------------------------------
+----------------------------------------- ## GENERATED FOR ROLE ## --------------------------------
+insert into role (id, role) values (1, "USER");
+insert into role (id, role) values (2, "ADMIN");
+
+----------------------------------------- ## GENERATED FOR PRODUCT ## --------------------------------
 insert into product (id, serial_number, description, name, price, quantity) values (3000, "KMNA239", "Apple iPhone 13 PRO", "iPhone 13 PRO", 1000, 5);
 insert into product (id, serial_number, description, name, price, quantity) values (3001, "PADMA232", "Samsung Galaxy S22", "Galaxy S22", 1100, 3);
 
------------------------------------------ ## GENERATED FOR RegistrationControllerIntTest ## --------------------------------
-insert into users(id,active, email, password, username) values(1000,1,"lucycar@email.com","9adm3av","lucycar");
+----------------------------------------- ## GENERATED FOR USER and CUSTOMER ## --------------------------------
+insert into users(id,active, email, password, username) values(1000,1,"lucycar@email.com","$2a$10$E.nGzmuccDIDu6ywkiZGXezuw/HulzC.czympJow0lAN.EGtavCeu","lucycar");
 insert into customer(id,first_name, last_name, user_id) values(1000,"Lucy","Car",(SELECT id FROM users WHERE email="lucycar@email.com"));
 insert into cart(id, total_price, customer_id) values(1000,0,(SELECT id FROM customer WHERE user_id=(SELECT id FROM users WHERE email="lucycar@email.com")));
+insert into users_roles (user_id, roles_id) values ((SELECT id FROM users WHERE username="lucycar"), (SELECT id FROM role WHERE role="ADMIN"));
 
-insert into users(id,active, email, password, username) values(2000,1,"billking@email.com","j7aad3","billking");
+
+insert into users(id,active, email, password, username) values(2000,1,"billking@email.com","2a$10$E.nGzmuccDIDu6ywkiZGXezuw/HulzC.czympJow0lAN.EGtavCeu","billking");
 insert into customer(id,first_name, last_name, user_id) values(2000,"Bill","King",(SELECT id FROM users WHERE email="billking@email.com"));
 insert into cart(id, total_price, customer_id) values(2000,0,(SELECT id FROM customer WHERE user_id=(SELECT id FROM users WHERE email="billking@email.com")));
 insert into cart_item (id, price, quantity, cart_id, product_id) values (2000,1000,1,
                                                                          (SELECT id FROM cart WHERE customer_id=((SELECT id FROM users WHERE email="billking@email.com"))),
                                                                          (SELECT id FROM product WHERE serial_number='KMNA239'));
+insert into users_roles (user_id, roles_id) values ((SELECT id FROM users WHERE username="lucycar"), (SELECT id FROM role WHERE role="USER"));
 
