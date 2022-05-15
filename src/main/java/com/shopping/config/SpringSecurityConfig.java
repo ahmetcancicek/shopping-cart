7874a424c991799a9d5a34f1c5d324ab613782db
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-15 17:16:24 +0300

    Fix SpringSecurityConfig

--- src/main/java/com/shopping/config/SpringSecurityConfig.java
+++ src/main/java/com/shopping/config/SpringSecurityConfig.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.annotation.Resource;
@@ -24,6 +26,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Resource(name = "userService")
     private final UserDetailsService userDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,6 +55,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         // Set permissions on endpoints
         http = http.authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/products/**").permitAll()
                 .anyRequest().authenticated()
                 .and();
 
@@ -59,6 +64,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 authenticationTokenFilterBean(),
                 UsernamePasswordAuthenticationFilter.class
         );
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 
     @Bean
