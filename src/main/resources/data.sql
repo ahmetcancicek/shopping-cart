d915a8072a15ec0c8eafef216ae220ad0c53caf9
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-15 22:43:16 +0300

    Implement initialize sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+----------------------------------------- ## GENERATED FOR ROLE ## --------------------------------
+insert into role (id, name) values (1, "USER");
+insert into role (id, name) values (2, "ADMIN");
