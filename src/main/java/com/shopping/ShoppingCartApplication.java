19a9182a48ed7e7ca4540ef354e8cce7ef39f47a
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-15 17:17:47 +0300

    Update ShoppingCartApplication

--- src/main/java/com/shopping/ShoppingCartApplication.java
+++ src/main/java/com/shopping/ShoppingCartApplication.java
@@ -1,8 +1,6 @@
 package com.shopping;
-;
-import com.shopping.repository.CustomerRepository;
+
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,40 +8,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+
 @SpringBootApplication
 @AllArgsConstructor
 public class ShoppingCartApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
-    }
-
-//    private final CustomerRepository customerRepository;
-
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-//                BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//                String hashedPassword = passwordEncoder.encode("12345");
-//                HashSet<Role> roles = new HashSet<>();
-//                roles.add(Role.builder().role("user").build());
-//
-//                Customer customer = Customer.builder()
-//                        .firstName("Steve")
-//                        .lastName("Clock")
-//                        .user(User.builder()
-//                                .email("Steveclock@email.com")
-//                                .username("steve")
-//                                .password(hashedPassword)
-//                                .roles(roles)
-//                                .active(true)
-//                                .build())
-//                        .build();
-//
-//                customerRepository.save(customer);
-            }
-        };
     }
 
     @Bean
