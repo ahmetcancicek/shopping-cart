17be64182ca8060e248b0511032a2ee1fd12924c
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-05-24 12:24:00 +0300

    Implement cart controller

--- src/main/java/com/shopping/controller/CartController.java
+++ src/main/java/com/shopping/controller/CartController.java
@@ -1,36 +1,77 @@
 package com.shopping.controller;
 
+import com.shopping.domain.dto.ApiResponse;
 import com.shopping.domain.dto.CartItemRequest;
 import com.shopping.domain.dto.CartResponse;
 import com.shopping.service.CartService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Set;
 
 
+@AllArgsConstructor
 @RestController
 public class CartController {
 
     private final CartService cartService;
 
-    public CartController(CartService cartService) {
-        this.cartService = cartService;
+
+    @PostMapping("/api/cart")
+    public ResponseEntity<ApiResponse<CartResponse>> addItemToCart(@Valid @RequestBody CartItemRequest cartItemRequest) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(HttpStatus.OK.value(), "The item has been added successfully", cartService.addItemToCart(cartItemRequest)));
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/carts")
-    public CartResponse addItemToCart(@Valid @RequestBody CartItemRequest cartItemRequest) {
-        return cartService.addItemToCart(cartItemRequest);
+    @PostMapping("/api/cart_bulk")
+    public ResponseEntity<ApiResponse<CartResponse>> addItemsToCart(@Valid @RequestBody Set<CartItemRequest> cartItemRequestList) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(HttpStatus.OK.value(), "The items has been added successfully", null));
     }
 
-    @GetMapping("/carts/{username}")
-    public CartResponse getCart(@PathVariable String username) {
-        return cartService.findByUsername(username);
+
+    @GetMapping("/api/cart")
+    public ResponseEntity<ApiResponse<CartResponse>> getCart() {
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(HttpStatus.OK.value(), "The cart has been got successfully", cartService.findByUsername(username)));
     }
 
-    @DeleteMapping("/carts/{username}")
-    public CartResponse deleteItemFromCart(@PathVariable String username, @RequestParam("serialNumber") String serialNumber) {
-        return cartService.deleteItemFromCart(username, serialNumber);
+    @DeleteMapping("/api/cart")
+    public ResponseEntity<ApiResponse<CartResponse>> deleteItemFromCart(@Valid @RequestBody CartItemRequest cartItemRequest) {
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(HttpStatus.OK.value(), "The item has been deleted successfully", cartService.deleteItemFromCart(username, cartItemRequest.getSerialNumber())));
+    }
+
+    @DeleteMapping("/api/cart/empty")
+    public ResponseEntity<ApiResponse<CartResponse>> clear() {
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(HttpStatus.OK.value(), "The items has been deleted successfully", cartService.clear(username)));
+    }
+
+    @PutMapping("/api/cart")
+    public ResponseEntity<ApiResponse<CartResponse>> updateItemFromCart(@Valid @RequestBody CartItemRequest cartItemRequest) {
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(HttpStatus.OK.value(), "The items has been updated successfully", cartService.updateItemFromCart(cartItemRequest)));
     }
 }
--- src/test/java/com/shopping/controller/CartControllerTest.java
+++ src/test/java/com/shopping/controller/CartControllerTest.java
@@ -7,9 +7,12 @@ import com.shopping.domain.dto.CartResponse;
 import com.shopping.service.CartService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,17 +27,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CartController.class)
-public class CartControllerTest {
-
-    @MockBean
-    private CartService cartService;
-
-    @Autowired
-    private ObjectMapper mapper;
+@AutoConfigureMockMvc
+public class CartControllerTest extends BaseControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private CartService cartService;
+    @Autowired
+    private ObjectMapper mapper;
 
+    private CartItemRequest cartItemRequest;
+    private CartResponse cartResponse;
+
+
+    @WithMockUser(username = "stevehouse", password = "GT380ABD", roles = {"ADMIN"})
     @Test
     public void it_should_add_item_to_cart() throws Exception {
         // given
@@ -51,7 +58,7 @@ public class CartControllerTest {
                 .items(new HashSet<>(Set.of(CartItemResponse.builder()
                         .price(BigDecimal.TEN)
                         .name("Egg")
-                        .description("Super egg")
+                        .description("Super Egg")
                         .quantity(1)
                         .serialNumber("Y5N3DJ")
                         .build())))
@@ -61,19 +68,82 @@ public class CartControllerTest {
 
         // when
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-                .post("/carts")
+                .post("/api/cart")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(cartItemRequest));
 
         // then
         mockMvc.perform(mockRequest)
-                .andExpect(status().isCreated())
                 .andDo(print())
-                .andExpect(jsonPath("$.totalQuantity").value(1))
-                .andExpect(jsonPath("$.username").value(cartResponse.getUsername()));
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.data.totalQuantity").value(1))
+                .andExpect(jsonPath("$.data.username").value(cartResponse.getUsername()))
+                .andExpect(jsonPath("$.data.totalPrice").value("10"))
+                .andExpect(jsonPath("$.data.items[0].serialNumber").value("Y5N3DJ"))
+                .andExpect(jsonPath("$.data.items[0].name").value("Egg"))
+                .andExpect(jsonPath("$.data.items[0].quantity").value("1"))
+                .andExpect(jsonPath("$.data.items[0].description").value("Super Egg"))
+                .andExpect(jsonPath("$.data.items[0].price").value("10"));
     }
 
+    @WithMockUser(username = "stevehouse", password = "GT380ABD", roles = {"ADMIN"})
+    @Test
+    public void it_should_add_items_to_cart() throws Exception {
+        // TODO: Implement below method to test
+        // given
+        Set<CartItemRequest> itemRequests = new HashSet<>(Set.of(
+                CartItemRequest.builder()
+                        .username("billking")
+                        .serialNumber("Y5N3DJ")
+                        .quantity(1)
+                        .build(),
+                CartItemRequest.builder()
+                        .username("billking")
+                        .serialNumber("HA7D80N")
+                        .quantity(2)
+                        .build()
+        ));
+
+        CartResponse cartResponse = CartResponse.builder()
+                .username("billking")
+                .totalQuantity(1)
+                .totalPrice(BigDecimal.TEN)
+                .items(new HashSet<>(Set.of(
+                        CartItemResponse.builder()
+                                .price(BigDecimal.TEN)
+                                .name("Egg")
+                                .description("Super Egg")
+                                .quantity(1)
+                                .serialNumber("Y5N3DJ")
+                                .build(),
+                        CartItemResponse.builder()
+                                .price(BigDecimal.TEN)
+                                .name("Egg")
+                                .description("Small Egg")
+                                .quantity(2)
+                                .serialNumber("HA7D80N")
+                                .build()
+                )))
+                .build();
+
+
+        // when
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .post("/api/cart_bulk")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(itemRequests));
+
+        // then
+        mockMvc.perform(mockRequest)
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.data.totalQuantity").value(1))
+                .andExpect(jsonPath("$.data.username").value(cartResponse.getUsername()));
+    }
+
+    @WithMockUser(username = "stevehouse", password = "GT380ABD", roles = {"ADMIN"})
     @Test
     public void it_should_return_cart_of_that_customer() throws Exception {
         // given
@@ -90,7 +160,7 @@ public class CartControllerTest {
                 .items(new HashSet<>(Set.of(CartItemResponse.builder()
                         .price(BigDecimal.TEN)
                         .name("Egg")
-                        .description("Super egg")
+                        .description("Super Egg")
                         .quantity(1)
                         .serialNumber("Y5N3DJ")
                         .build())))
@@ -100,15 +170,26 @@ public class CartControllerTest {
 
         // when
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-                .get("/carts/{username}", cartResponse.getUsername())
-                .accept(MediaType.APPLICATION_JSON);
+                .get("/api/cart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(cartItemRequest));
 
         // then
         mockMvc.perform(mockRequest)
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username").value(cartResponse.getUsername()));
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.data.totalQuantity").value(1))
+                .andExpect(jsonPath("$.data.username").value(cartResponse.getUsername()))
+                .andExpect(jsonPath("$.data.totalPrice").value("10"))
+                .andExpect(jsonPath("$.data.items[0].serialNumber").value("Y5N3DJ"))
+                .andExpect(jsonPath("$.data.items[0].name").value("Egg"))
+                .andExpect(jsonPath("$.data.items[0].quantity").value("1"))
+                .andExpect(jsonPath("$.data.items[0].description").value("Super Egg"))
+                .andExpect(jsonPath("$.data.items[0].price").value("10"));
     }
 
+    @WithMockUser(username = "stevehouse", password = "GT380ABD", roles = {"ADMIN"})
     @Test
     public void it_should_delete_item_from_cart() throws Exception {
         // given
@@ -125,19 +206,95 @@ public class CartControllerTest {
                 .items(new HashSet<>())
                 .build();
 
-        given(cartService.deleteItemFromCart(cartItemRequest.getUsername(), cartItemRequest.getSerialNumber())).willReturn(cartResponse);
+        given(cartService.deleteItemFromCart(any(), any())).willReturn(cartResponse);
 
         // when
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-                .delete("/carts/{username}?serialNumber={serialNumber}", cartItemRequest.getUsername(), cartItemRequest.getSerialNumber())
+                .delete("/api/cart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(cartItemRequest));
+
+
+        // then
+        mockMvc.perform(mockRequest)
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.data.totalQuantity").value(0))
+                .andExpect(jsonPath("$.data.username").value(cartResponse.getUsername()))
+                .andExpect(jsonPath("$.data.totalPrice").value("0"));
+    }
+
+    @WithMockUser(username = "stevehouse", password = "GT380ABD", roles = {"ADMIN"})
+    @Test
+    public void it_should_delete_all_items_from_cart() throws Exception {
+        // given
+        CartResponse cartResponse = CartResponse.builder()
+                .username("billking")
+                .totalQuantity(0)
+                .totalPrice(BigDecimal.ZERO)
+                .items(new HashSet<>())
+                .build();
+
+        given(cartService.clear(any())).willReturn(cartResponse);
+
+        // when
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .delete("/api/cart/empty")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON);
 
         // then
         mockMvc.perform(mockRequest)
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username").value(cartResponse.getUsername()))
-                .andExpect(jsonPath("$.totalQuantity").value("0"))
-                .andDo(print());
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.data.totalQuantity").value(0))
+                .andExpect(jsonPath("$.data.username").value(cartResponse.getUsername()))
+                .andExpect(jsonPath("$.data.totalPrice").value("0"));
+    }
+
+    @WithMockUser(username = "stevehouse", password = "GT380ABD", roles = {"ADMIN"})
+    @Test
+    public void it_should_update_item_from_cart() throws Exception {
+        // given
+        CartItemRequest cartItemRequest = CartItemRequest.builder()
+                .username("billking")
+                .serialNumber("Y5N3DJ")
+                .quantity(1)
+                .build();
+
+        CartResponse cartResponse = CartResponse.builder()
+                .username("billking")
+                .totalQuantity(1)
+                .totalPrice(BigDecimal.TEN)
+                .items(new HashSet<>(Set.of(CartItemResponse.builder()
+                        .price(BigDecimal.TEN)
+                        .name("Egg")
+                        .description("Super Egg")
+                        .quantity(1)
+                        .serialNumber("Y5N3DJ")
+                        .build())))
+                .build();
+
+        given(cartService.updateItemFromCart(any())).willReturn(cartResponse);
+
+        // when
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .put("/api/cart")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(cartItemRequest));
+
+        // then
+        mockMvc.perform(mockRequest)
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.data.totalQuantity").value(1))
+                .andExpect(jsonPath("$.data.username").value(cartResponse.getUsername()))
+                .andExpect(jsonPath("$.data.totalPrice").value("10"))
+                .andExpect(jsonPath("$.data.items[0].serialNumber").value("Y5N3DJ"))
+                .andExpect(jsonPath("$.data.items[0].name").value("Egg"))
+                .andExpect(jsonPath("$.data.items[0].quantity").value("1"))
+                .andExpect(jsonPath("$.data.items[0].description").value("Super Egg"))
+                .andExpect(jsonPath("$.data.items[0].price").value("10"));
     }
 }
