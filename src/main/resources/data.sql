34c2f168688f2b9bf679193adae33da575286286
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-07-02 12:54:07 +0300

    Fix sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,18 @@
 INSERT IGNORE INTO role(id, name)
-VALUES (1, 'USER');
+VALUES (1, 'ROLE_USER');
 
 INSERT IGNORE INTO role(id, name)
-VALUES (2, 'ADMIN');
-
-
+VALUES (2, 'ROLE_ADMIN');
 
 INSERT IGNORE INTO users(id, active, email, password, username)
-VALUES (1, 1, "georgehouse@email.com", "$2a$10$E.nGzmuccDIDu6ywkiZGXezuw/HulzC.czympJow0lAN.EGtavCeu", "georgehouse");
+VALUES (1, 1, 'georgehouse@email.com', '$2a$10$OOmD41D50F4SufvNJN.Sde/Y/RjujIiDIBCON0t0.5cHRZlbYWhpq', 'georgehouse');
 
 INSERT IGNORE INTO customer(id, first_name, last_name, user_id)
-VALUES (1, "George", "House", (SELECT id FROM users WHERE email = "georgehouse@email.com"));
+VALUES (1, 'George', 'House', (SELECT id FROM users WHERE email = 'georgehouse@email.com'));
 
 INSERT IGNORE INTO cart(id, total_price, customer_id)
-VALUES (1, 0, (SELECT id FROM customer WHERE user_id = (SELECT id FROM users WHERE email = "georgehouse@email.com")));
+VALUES (1, 0, (SELECT id FROM customer WHERE user_id = (SELECT id FROM users WHERE email = 'georgehouse@email.com')));
 
 INSERT IGNORE INTO users_roles (user_id, roles_id)
-VALUES ((SELECT id FROM users WHERE username = "georgehouse"), (SELECT id FROM role WHERE name = "ADMIN"));
+VALUES ((SELECT id FROM users WHERE username = 'georgehouse'), (SELECT id FROM role WHERE name = 'ROLE_ADMIN'));
 
