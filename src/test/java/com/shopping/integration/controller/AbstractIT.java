e78ced73e625db33b47973cdb350d16a58c51595
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-07-16 17:25:20 +0300

    Fix configuration

--- src/test/java/com/shopping/integration/controller/AbstractIT.java
+++ src/test/java/com/shopping/integration/controller/AbstractIT.java
@@ -78,7 +78,7 @@ public abstract class AbstractIT {
     public static void properties(DynamicPropertyRegistry registry) {
         registry.add("spring.sql.init.mode", () -> " always");
         registry.add("spring.jpa.defer-datasource-initialization", () -> "true");
-        registry.add("spring.jpa.hibernate.dll-auto", () -> "create-drop");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
         registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.MySQL8Dialect");
         registry.add("spring.datasource.initialization-mode", () -> "never");
         registry.add("spring.datasource.url", mysql::getJdbcUrl);
