4c7c5ee5cf13f71535704eaaa97d3a9275d67517
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-07-22 12:35:43 +0300

    Fix project name for test

---
+++ src/test/java/com/shopping/EcommerceStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping;
+
+import com.shopping.integration.controller.AbstractIT;
+import org.junit.jupiter.api.Test;
+
+@IT
+public class EcommerceStarterApplicationTests extends AbstractIT {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
